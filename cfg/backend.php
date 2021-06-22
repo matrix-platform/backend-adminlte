8a01c351346fe8a3e15fbafc39c51b2d26ef17ec
jrealm <jrealm@gmail.com>
2021-06-22 12:24:55 +0800

    data list

--- cfg/backend.php
+++ cfg/backend.php
@@ -2,18 +2,22 @@
 
 return [
 
+    'button-search' => 'btn btn-default',
+    'button-view' => 'btn btn-default',
+
+    'icon-delete' => 'fas fa-trash-alt',
+    'icon-export' => 'fas fa-file-export',
+    'icon-new' => 'fas fa-plus',
+    'icon-search' => 'fas fa-search',
+    'icon-view' => 'fas fa-edit',
+
     'index' => 'backend/index.twig',
-
     'label' => 'backend/label.twig',
-
     'label-updated' => 'backend/label-updated.twig',
-
+    'list' => 'backend/list.twig',
     'login-form' => 'backend/login-form.twig',
-
     'overview' => 'backend/overview.twig',
-
     'password-changed' => 'backend/password-changed.twig',
-
     'password-form' => 'backend/password-form.twig',
 
 ];
