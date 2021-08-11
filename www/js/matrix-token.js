4af5821832d348df0b2d811688c9114ef256a12e
jrealm <jrealm@gmail.com>
2021-08-11 10:44:14 +0800

    compatible with http

--- www/js/matrix-token.js
+++ www/js/matrix-token.js
@@ -8,7 +8,11 @@
     $(document).ajaxSend(function (ignore, request) {
         var token = [0, 0, 0, 0, 0, 0].map(() => Math.random().toString(36).substring(2)).join("");
 
-        document.cookie = `matrix-token=${token}; path=/; samesite=strict; secure`;
+        if (location.protocol === "https") {
+            document.cookie = `matrix-token=${token}; path=/; samesite=strict; secure`;
+        } else {
+            document.cookie = `matrix-token=${token}; path=/`;
+        }
 
         request.setRequestHeader("matrix-token", token);
     });
